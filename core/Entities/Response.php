463496fc55e70ab7f1b2769d2a802333b7bd519a
Mumtoz Kodirov <imumtoz@gmail.com>
2021-07-19 18:38:44 +0500

    added Entity\Response class;

---
+++ core/Entities/Response.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Core\Entities;
+
+class Response
+{
+    private $content;
+    private $headers = [];
+
+    public function addHeader($header)
+    {
+        $this->headers[] = $header;
+    }
+
+    public function addHeaders($headers)
+    {
+        $this->headers = array_merge($this->headers, $headers);
+    }
+
+    public function redirect($url, $status = 302)
+    {
+        header('Location: ' . str_replace(['&amp;', "\n", "\r"], ['&', '', ''], $url), true, $status);
+        exit();
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function output()
+    {
+        if (!headers_sent()) {
+            foreach ($this->headers as $header) {
+                header($header, true);
+            }
+        }
+
+        if (is_array($this->content)) {
+            json_encode($this->content);
+            header('Content-type', 'text/json');
+        } elseif (is_string($this->content)) {
+            echo $this->content;
+        } else {
+            var_dump($this->content);
+        }
+    }
+}
