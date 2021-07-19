ad8b7d50916981c8d1112a2128802d7dcb45ccd9
Mumtoz Kodirov <imumtoz@gmail.com>
2021-07-19 19:19:39 +0500

    models added: User, Task.

---
+++ app/Models/Task.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Core\Engine\Model;
+
+class Task extends Model
+{
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Core\Engine\Model;
+
+class User extends Model
+{
+
+}
