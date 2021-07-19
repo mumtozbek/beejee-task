9baf2d043571243f7989d876d4ba3992b461ae38
Mumtoz Kodirov <imumtoz@gmail.com>
2021-07-19 19:39:35 +0500

    database connection default config added.

--- app/config.php
+++ app/config.php
@@ -3,4 +3,9 @@ return [
     'debug' => true,
     'maintenance' => false,
     'default_route' => 'auth',
+
+    'db.host' => '127.0.0.1',
+    'db.user' => 'root',
+    'db.pass' => '',
+    'db.base' => 'beejee',
 ];
